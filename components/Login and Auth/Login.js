9fa7e94001121d35cb923c078fa64af27d6a365f
Kimberly Benton <kimberly.d.benton@gmail.com>
2023-08-20 12:20:11 -0400

    changes to login almost complete - error with login variable

--- components/Login and Auth/Login.js
+++ components/Login and Auth/Login.js
@@ -1,23 +1,33 @@
-import { View, TextInput, Button, Text } from 'react-native'
+import { View, TextInput, Button, Text, Pressable } from 'react-native'
 import { useState, useContext } from 'react'
 import { AuthContext } from '../context/AuthContext'
 
 function Login() {
 
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
 
+    const login = useContext(AuthContext)
 
     return(
         <View>
+            <Text>Lets Cancel Some Plans</Text>
             <TextInput
                 placeholder = 'username'
-            >
-            </TextInput>
+                value={username}
+                autoCapitalize='none'
+                onChangeText={text => setUsername(text)}
+            />
             <TextInput
                 placeholder = 'password'
-            >
-            </TextInput>
+                value={password}
+                autoCapitalize='none'
+                secureTextEntry={true}
+                onChangeText={text => setPassword(text)}
+            />
             <Button
-                title = 'lets cancel some plans'
+                title = 'Ready to Recharge'
+                onPress={() => {login(username, password)}}
             >
             </Button>
         </View>
